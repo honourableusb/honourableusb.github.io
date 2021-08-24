4430cfcd69b87ff26816c9ab9cb6ea49f9c982d2
Matthew Haskell <matthew.haskell229@gmail.com>
2021-08-24 15:01:48 -0500

    website - trying to get routing to work w gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import {RouterLink} from '';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+
+
+
+  public gotoHome(): void {
+    console.log("abcdef");
+  }
+
+
 }
