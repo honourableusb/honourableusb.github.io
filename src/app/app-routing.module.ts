f0a71dc2769333492ef0d9b6db6d440448ce6eb0
Matthew Haskell <matthew.haskell229@gmail.com>
2021-08-24 21:40:25 -0500

    making changes for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-                        { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }];
+                        { path: '', loadChildren: () => import('./home/home.module').then(m=>m.HomeModule) },
+                        { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+                        { path: '**', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
